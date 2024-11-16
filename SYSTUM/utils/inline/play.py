28e830a74b13dcb0f68f52c1960516813c6c8bdf
istkharalam6202 <istkharalam6202@gmail.com>
2024-11-16 21:04:57 +0530

    Update play.py

--- SYSTUM/utils/inline/play.py
+++ SYSTUM/utils/inline/play.py
@@ -69,10 +69,10 @@ def stream_markup_timer(_, chat_id, played, dur):
         ],
       [
             InlineKeyboardButton(
-                text="ᴏᴡɴᴇʀ", url="https://t.me/ll_THUNDER_lll",
+                text="ᴏᴡɴᴇʀ", url="https://t.me/VIP_RAM_HACKER_TG",
             ),
             InlineKeyboardButton(
-                text="sᴜᴩᴩᴏʀᴛ", url="https://t.me/+QExD9mtKnz5hNjY1",
+                text="sᴜᴩᴩᴏʀᴛ", url="https://t.me/+nSBmSAg-Lbs4MDdl",
             )
         ],
          [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
