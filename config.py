9edcf11b79f9f26bf50577bce8624155761c50e1
teamrupak <rupak9315238756@gmail.com>
2024-11-22 19:27:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -41,7 +41,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/istkharalam6202/SYSTUM",
+    "https://github.com/teamrupak/Rupak-",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
